48afa4c85698e9d4d47b2e5d1ba9ae07fa08eda8
Rinat <RinatProgrammer@gmail.com>
2024-06-23 21:55:42 +0300

    all works
    right now all works after
    
    python manage.py makemigrations
    and
    python manage.py migrate

--- todo/models.py
+++ todo/models.py
@@ -1,10 +1,6 @@
-# todo/models.py
 from django.db import models
 
 class TodoItem(models.Model):
     title = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField()  
     completed = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.title
